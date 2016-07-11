5895266b0d7c3f499f9168ebdab2121e86a024a6
LM <lewlimin@gmail.com>
2016-07-11 20:35:23 +0800

    added project api

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+const Project = require('./project')
 //its an object because there is a .
 //node uses port 3000.
 
@@ -23,6 +24,16 @@ app.get('/profile', (req,res) => {
   res.status(200).json(name)
 })
 
+app.get('/projects', (req,res) => {
+  const project1 = new Project('aboutme','this is a project about me', 'google.com', 'cat')
+  const project2 = new Project('aboutme','this is a project about x', 'google.com', 'cat')
+  const project3 = new Project('aboutme','this is a project about y', 'google.com', 'cat')
+  const project4 = new Project('aboutme','this is a project about z', 'google.com', 'cat')
+  const project5 = new Project('aboutme','this is a project about o', 'google.com', 'cat')
+  const arrayOfProjects = [project1, project2, project3, project4]
+  res.status(200).json(arrayOfProjects)
+})
+
 app.get('/hello', (req,res,next) => {
   res.json({message: 'world'})
 })
@@ -30,8 +41,6 @@ app.get('/hello', (req,res,next) => {
 app.get('/profile', (req,res) => {
   res.json({message: 'world'})
 })
-
-
 
 
 app.listen(port, () => {
