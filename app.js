b8dd8d4d37ddfd521c3646fe02cd3dc52f951052
LM <lewlimin@gmail.com>
2016-07-11 20:21:28 +0800

    v2

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
-
 //its an object because there is a .
 //node uses port 3000.
-app.listen(port, function(){
+
+//request and response
+//telling the app to lsiten to get request.
+//req = response, res = response, next
+
+app.use('/', (req,res,next) => {
+  console.log('hi')
+  next()
+})
+
+app.get('/profile', (req,res) => {
+  const name = {
+    name: 'Li Min',
+    age: 31,
+    pet: 'nil',
+    gender: 'male'
+  }
+  res.status(200).json(name)
+})
+
+app.get('/hello', (req,res,next) => {
+  res.json({message: 'world'})
+})
+
+app.get('/profile', (req,res) => {
+  res.json({message: 'world'})
+})
+
+
+
+
+app.listen(port, () => {
   console.log(`listening to port ${port}`)
 })
